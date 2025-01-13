0fed1f2c12ccbf3c3a238e4197bab560932f2bb1
Sadman Sajid <sadman-sajid@khan.tail26ac73.ts.net>
2025-01-13 18:39:02 +0600

    updating server file in Prod

--- server.js
+++ server.js
@@ -2,25 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Default to 3000
+const MONGO_URI = process.env.MONGO_URI; // Default MongoDB URI
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/users', {
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+}).then(() => console.log(`MongoDB connected to ${MONGO_URI}`))
   .catch(err => console.log(err));
 
 // Define User schema
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
-    type: String,
 });
 
 const User = mongoose.model('User', userSchema);
@@ -35,7 +37,7 @@ app.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ username, password });
         if (user) {
-            res.json({ success: true, message: `Hello from ${user.type}` });
+            res.json({ success: true, message: 'User exists' });
         } else {
             res.json({ success: false, message: 'User not found' });
         }
